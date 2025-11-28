82851bdc5c235986d9950cbf19a25737876218bf
Josivan Tarcio <josivantarcio@gmail.com>
2025-11-27 21:04:55 -0300

    Adicionando backend ao repositório

---
+++ backend/closetRental/src/main/java/com/jtarcio/closetrental/ClosetRentalApplication.java
@@ -0,0 +1,13 @@
+package com.jtarcio.closetrental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClosetRentalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClosetRentalApplication.class, args);
+    }
+
+}
---
+++ backend/closetRental/src/test/java/com/jtarcio/closetrental/ClosetRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jtarcio.closetrental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClosetRentalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
